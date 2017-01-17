a56f6e413ecd6398a556823b0473d6cd097f514e
Uriel Coria <u.coria@outlook.com>
2017-01-17 12:56:37 -0600

    Uriel to master (#3)
    * added new logic for the conf parser
    
    * adding activity chart and so
    
    * fixed minor bugs
    
    * so, no erros, but no printing WTF!
    
    * added success message
    
    * changed -a to -A
    
    * calling main now
    
    * config function added

--- nonova.py
+++ nonova.py
@@ -1,22 +1,20 @@
 #/bin/python
 
 #PREREQUISITES TO RUN THIS THING
+# -Python27
 # -ConfigParser
+# -ArgParse
 # -nova-cli? or will it be hosted in tchai? if tchai true, we need to make it public
 import os
 import sys
-#https://docs.python.org/2/library/configparser.html
+## https://docs.python.org/2/library/configparser.html
 from configparser import ConfigParser
 from argparse import ArgumentParser
 
-
-
-
 class NoNovaConfigParser(ConfigParser):
-    def __init__(self):
-        super(self).__init__()
-        self.configp = configparser.ConfigParser()
-        self.nonovaconfigfile="nonovaconfig.ini"
+    def __init__(self, conf_file):
+        self.configp = ConfigParser()
+        self.nonovaconfigfile = conf_file
         # if (os.path.isfile(self.nonovaconfigfile)):
         #     print("%s file found" % self.nonovaconfigfile)
         #     self.configp.read(self.nonovaconfigfile)
@@ -30,17 +28,16 @@ class NoNovaConfigParser(ConfigParser):
             self.password = raw_input("What's your password? Pinky promise we won't share it")
             print ("thanks that's all, we'll let you know when is finished")
             self.configp.add_section("Credentials")
-            self.configp.set("Credentials","user",user)
-            self.configp.set("Credentials","pass",password)
+            self.configp.set("Credentials","user",self.user)
+            self.configp.set("Credentials","pass",self.password)
             with open(self.nonovaconfigfile, "wb") as config_file:
                 self.configp.write(config_file)
-
+            print "Config file created!"
 
 def cli_parser():
     parser = ArgumentParser(description = """Command line helper for filling nova""")
-    parser.add_argument("-c" " --config", dest="config", help="Config file", type=config)
-    parser.add_argument("-A" " --de-actividades",
-    help="Print the activity arguments with ID")
+    parser.add_argument("-c" " --config", dest="config", help="Config file", type=str)
+    parser.add_argument("-A" ,dest="activity", help="Print the activity arguments with ID",action="store_true")
 #    parser.add_argument("-P", "--de-proyectos",
     #help="Print your personal project arguments with ID, This requires CONF FILE ")
     parser.add_argument("--papu", dest="papu", default='saca el pack papu', action="store", type=str)
@@ -48,17 +45,34 @@ def cli_parser():
         parser.print_help()
         sys.exit(1)
     args = parser.parse_args()
-    #return args
+    return args
 
-def config():
+def config(args):
     # Verify conf file exists, else create_cofig
-    confparse = NoNovaConfigParser()
+    confparse = NoNovaConfigParser(args.config)
+    if not os.path.exists(confparse.nonovaconfigfile):
+        confparse.nonova_create_config()
+    else:
+        confparse.read(confparsenonovaconfigfile)
+        user = confparse.get("Credentials","user")
+        password = confparse.get("Credentials","pass")
+        print "Your credentials are {} and {}".format(user, password)
+        # Login to nova CLI using Credentials :D
+        # Maybe a success message
 
-    confparse.nonova_create_config()
-
+def activity():
+    print "You will be required the activity ID"
+    print "------------------------------------"
+    actividades = {1:"Coding",2:"Meeting w/Client",3:"Design",4:"Meeting",5:"Support",6:"Training",7:"Fix/Debug",8:"Project Hours",9:"Research",11:"Project Review",12:"Project Management",13:"Architect",14:"Testing / QA",15:"Estimation",16:"PTO",17:"Holiday",18:"Analysis",19:"UI Graphic Design",20:"Code Redesign",21:"iTexico MX Task",23:"Interviewing",24:"Recruting",25:"Documentation",26:"Environment Setup",27:"Technical Advising",28:"TL",29:"Vacation"}
+    for key in actividades:
+        print "{} ...... {}".format(key, actividades[key])
 
 def main():
-    cli_parser()
+    args = cli_parser()
+    if args.activity:
+        activity()
+    if args.config:
+        config(args)
 
 if __name__ == "__main__":
-    sys.exit(cli_parser())
+    sys.exit(main())
