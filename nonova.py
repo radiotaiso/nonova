e30590f6f3cbbe103a872cc91ff5bc39f44710c3
Antonio Herran <aherran@itexico.net>
2017-01-23 10:55:14 -0600

    confparser class now uses args

--- nonova.py
+++ nonova.py
@@ -13,35 +13,38 @@ from configparser import ConfigParser
 from argparse import ArgumentParser
 
 class NoNovaConfigParser(ConfigParser):
-    def __init__(self, conf_file):
+    def __init__(self, args):
         self.configp = ConfigParser()
-        self.nonovaconfigfile = conf_file
-    def nonova_create_config(self):
-            print ("Creating new text file...")
-            print ("-------------------------")
-            self.user = raw_input("What's your username?: ")
-            self.password = raw_input("What's your password? Pinky promise we won't share it: ")
-            print ("Thanks that's all, we'll let you know when is finished")
-            self.configp.add_section("Credentials")
-            self.configp.set("Credentials","user",self.user)
-            self.configp.set("Credentials","pass",self.password)
-            self.osPlatform = ""
-            self.osPlatform = platform.system()
-            self.configp.set("Credentials","OS",self.osPlatform)
+        self.args = args
+        self.config_file = args.config
 
-            with open(self.nonovaconfigfile, "wb") as config_file:
-                self.configp.write(config_file)
-            print "Config file created!"
-            print "Remember use the -p option to fetch your projects! You only need to do this once!"
+    def nonova_create_config(self):
+        print ("Creating new text file...")
+        print ("-------------------------")
+        self.user = raw_input("What's your username?: ")
+        self.password = raw_input("What's your password? Pinky promise we won't share it: ")
+        print ("Thanks that's all, we'll let you know when is finished")
+        self.configp.add_section("Credentials")
+        self.configp.set("Credentials","user",self.user)
+        self.configp.set("Credentials","pass",self.password)
+        self.osPlatform = ""
+        self.osPlatform = platform.system()
+        self.configp.set("Credentials","OS",self.osPlatform)
+        with open(self.args.config, "wb") as config_file:
+            self.configp.write(config_file)
+        print "Config file created!"
+        print "Remember use the -p option to fetch your projects! You only need to do this once!"
+
     def nonova_confirm_config(self):
-            self.configp.read(self.nonovaconfigfile)
+            self.configp.read(self.args.config)
             print "File Found!"
             self.userConfirm = self.configp.get("Credentials","user")
             self.passConfirm = self.configp.get("Credentials","pass")
             self.osPlatformC = self.configp.get("Credentials","os")
             print "Your credentials are {} and {} and currently running on {}".format(self.userConfirm, self.passConfirm,self.osPlatformC)
+
     def nonova_get_projects(self):
-            self.configp.read(self.nonovaconfigfile)
+            self.configp.read(self.args.config)
             self.userConfirm = self.configp.get("Credentials","user")
             self.passConfirm = self.configp.get("Credentials","pass")
             self.osPlatform = self.configp.get("Credentials","os")
@@ -54,15 +57,15 @@ class NoNovaConfigParser(ConfigParser):
                  pathToCli="/bin/osx/nova-cli"
             elif osPlatform == "Linux" or "Linux2":
                  pathToCli="/bin/linux"
-
-
 # --------- ends NoNovaConfigParser class -----------------
+
+
 # --------- Arg Parser arguments --------------------------
 def cli_parser():
     parser = ArgumentParser(description = """Command line helper for filling nova""")
     parser.add_argument("-c" " --config", dest="config", help="You must run this only the first time", type=str)
-    parser.add_argument("-p" " --project", dest="getp", help="Update your project list", type=str)
-    parser.add_argument("-A" ,dest="activity", help="Print the activity arguments with ID",action="store_true")
+    parser.add_argument("-p" " --project", dest="getp", help="Update your project list", action="store_true")
+    parser.add_argument("-A" " --activity", dest="activity", help="Print the activity arguments with ID", action="store_true")
     # parser.add_argument("-P", "--de-proyectos",
     # help="Print your personal project arguments with ID, This requires CONF FILE ")
     # parser.add_argument("--papu", dest="papu", help="summon papu" default="saca el pack papu", action="store", type=str)
@@ -71,18 +74,19 @@ def cli_parser():
         sys.exit(1)
     args = parser.parse_args()
     return args
+
 # ---- Arg Parser Options! ------------------------------------------
 def config(args):
     # Verify conf file exists, else create_cofig
-    if not os.path.exists(confparse.nonovaconfigfile):
+    if not os.path.exists(args.config):
         confparse.nonova_create_config()
     else:
-        print confparse.nonovaconfigfile
+        print args.config
         confparse.nonova_confirm_config()
 
 def update_projects(args):
     print "si entra al update_projects"
-    print confparse.nonovaconfigfile
+    print confparse.config_file
     # if not os.path.exists(confparse.nonovaconfigfile)::
     #     print "config.ini was not found, you must create one first."
     #     confparse.nonova_create_config()
