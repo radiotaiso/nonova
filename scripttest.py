9942abda5552e69a9cbde895a908e05683cb0627
ucoria-itexico <u.coria@outlook.com>
2017-03-31 16:21:13 -0600

    faltaba un puto :

--- scripttest.py
+++ scripttest.py
@@ -2,7 +2,7 @@ from scripttest import TestFileEnvironment
 
 env = TestFileEnvironment('./tests')
 
-def test_script()
+def test_script():
     env.reset()
     result = env.run('python nonova.py -a')
     assert result.stdout.startswith('Creating new text file...')
